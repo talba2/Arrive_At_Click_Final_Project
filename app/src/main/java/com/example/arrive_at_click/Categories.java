a6d23ddc12c1168e610eaba1fe63b0705397b975
talba2 <talba2@ac.sce.ac.il>
2018-03-13 22:03:33 +0200

    initial commit

---
+++ app/src/main/java/com/example/arrive_at_click/Categories.java
@@ -0,0 +1,14 @@
+package com.example.arrive_at_click;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
---
+++ app/src/main/java/com/example/arrive_at_click/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.arrive_at_click;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvWelcome = (TextView) findViewById(R.id.tvWelcome);
+        //set the font size 50
+        tvWelcome.setTextSize(60);
+
+        TextView bttnCategory = (TextView) findViewById(R.id.bttnCategory);
+        //set the font size 25
+        bttnCategory.setTextSize(25);
+
+        TextView tvSearch = (TextView) findViewById(R.id.tvSearch);
+        //set the font size 20
+        tvSearch.setTextSize(20);
+    }
+
+    public void bttnCategory(View v)
+    {
+        Intent i = new Intent(MainActivity.this,Categories.class);
+        startActivity(i);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
